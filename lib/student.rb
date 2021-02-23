8d64357a26a0acfdff82cf76d0f1afa59328ec0c
yves <yvesmomplais@hotmail.com>
2021-02-23 01:03:11 -0500

    commit tree d1326127626306bca557236bb4f6b09d4dc614c6

---
+++ lib/student.rb
@@ -0,0 +1,11 @@
+class Student
+    def hello
+        phrase =  "Hey there! I'm so excited to learn stuff."
+        puts phrase
+    end
+
+    def raise_hand
+        phrase = "Pick me!"
+        puts phrase
+    end
+end
